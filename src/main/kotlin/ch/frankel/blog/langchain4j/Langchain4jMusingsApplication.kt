13fe9d17fb6618a78a1b140391d6d4ff6e725fb4
Nicolas Fränkel <nicolas@frankel.ch>
2024-10-09 20:03:48 +0200

    Switch from regular model to streaming model

--- src/main/kotlin/ch/frankel/blog/langchain4j/Langchain4jMusingsApplication.kt
+++ src/main/kotlin/ch/frankel/blog/langchain4j/Langchain4jMusingsApplication.kt
@@ -1,34 +1,50 @@
 package ch.frankel.blog.langchain4j
 
-import dev.langchain4j.model.chat.ChatLanguageModel
+import dev.langchain4j.data.message.AiMessage
+import dev.langchain4j.model.StreamingResponseHandler
+import dev.langchain4j.model.chat.StreamingChatLanguageModel
+import kotlinx.coroutines.reactive.asFlow
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.support.beans
-import org.springframework.web.servlet.function.ServerRequest
-import org.springframework.web.servlet.function.ServerResponse
-import org.springframework.web.servlet.function.ServerResponse.ok
-import org.springframework.web.servlet.function.router
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.awaitBody
+import org.springframework.web.reactive.function.server.bodyAndAwait
+import org.springframework.web.reactive.function.server.coRouter
+import reactor.core.publisher.Sinks
 
 @SpringBootApplication
 class Langchain4jMusingsApplication
 
-class PromptHandler(private val model: ChatLanguageModel) {
+class AppStreamingResponseHandler(private val sink: Sinks.Many<String>) : StreamingResponseHandler<AiMessage> {
 
-    fun handle(req: ServerRequest): ServerResponse {
-        val prompt = req.body<String>(String::class.java)
-        val result = model.generate(prompt)
-        return ok().body(result)
+    override fun onNext(token: String) {
+        sink.tryEmitNext(token)
+    }
+
+    override fun onError(error: Throwable) {
+        sink.tryEmitError(error)
+    }
+}
+
+class PromptHandler(private val model: StreamingChatLanguageModel) {
+
+    suspend fun handle(req: ServerRequest): ServerResponse {
+        val prompt = req.awaitBody<String>()
+        val sink = Sinks.many().unicast().onBackpressureBuffer<String>()
+        model.generate(prompt, AppStreamingResponseHandler(sink))
+        return ServerResponse.ok().bodyAndAwait(sink.asFlux().asFlow())
     }
 }
 
 fun beans() = beans {
     bean {
-        router {
-            POST("/")(PromptHandler(ref<ChatLanguageModel>())::handle)
+        coRouter {
+            POST("/")(PromptHandler(ref<StreamingChatLanguageModel>())::handle)
         }
     }
 }
-
 
 fun main(args: Array<String>) {
     runApplication<Langchain4jMusingsApplication>(*args) {
